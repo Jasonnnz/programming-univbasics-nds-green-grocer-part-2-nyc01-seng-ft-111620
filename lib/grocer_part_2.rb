76ad3103e5f8100e8dbcd9ea15f3b67b32196ca7
Jason  Zhou <jzhou35@buffalo.edu>
2020-11-12 02:44:01 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,12 +4,29 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    name = coupon[:item] 
+    cost_per = coupon[:cost]/coupon[:num]
+    cart.each do |item|
+      if item[:item] == name && item[:count] >= coupon[:num]
+        cart << {:item => "#{name} W/COUPON", :price => cost_per, :clearance => item[:clearance], :count => coupon[:num]}
+        item[:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.each do |item|
+    if item[:clearance] 
+      item[:price] -= (item[:price] * 0.20)
+    end
+  end
+  cart      
 end
 
 def checkout(cart, coupons)
@@ -22,4 +39,15 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  con_cart = consolidate_cart(cart)
+  apply_coupons(con_cart, coupons)
+  apply_clearance(con_cart)
+  grand_total = 0 
+  con_cart.each do |item|
+    grand_total += (item[:price] * item[:count])
+  end
+  if grand_total >= 100
+    grand_total -= (grand_total * 0.1) 
+  end
+  grand_total
 end
